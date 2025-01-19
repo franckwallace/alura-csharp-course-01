e4f1c6d4548d4734e804aa3168c03768e98a1816
Franck Wallace <franckwallace.fw@outlook.com>
2025-01-18 23:19:52 -0300

    Criação do 02-ByteBank

--- 01-ByteBank/Program.cs
+++ 01-ByteBank/Program.cs
@@ -10,7 +10,26 @@ namespace _01_ByteBank
     {
         static void Main(string[] args)
         {
-            new ContaCorrente();
+            //instanciando (ou criando um objeto) a partir da classe ContaCorrente
+
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+
+            //acessando os parâmetros do objeto e atribuindo valores
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 863;
+            contaDaGabriela.numero = 863123;
+            contaDaGabriela.saldo = 100;
+
+            Console.WriteLine("Titular: " + contaDaGabriela.titular);
+            Console.WriteLine("Agencia: " + contaDaGabriela.agencia);
+            Console.WriteLine("Número: " + contaDaGabriela.numero);
+            Console.WriteLine("Saldo: " + contaDaGabriela.saldo);
+
+            contaDaGabriela.saldo += 200;
+
+            Console.WriteLine("Saldo: " + contaDaGabriela.saldo);
+
+
 
             Console.ReadLine();
 
