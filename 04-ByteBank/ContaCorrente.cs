c8405170a45c9f2d4b8bf3152517dc3771c3fb63
Franck Wallace <franckwallace.fw@outlook.com>
2025-01-21 23:50:55 -0300

    04-ByteBank: criação da função Sacar

---
+++ 04-ByteBank/ContaCorrente.cs
@@ -0,0 +1,25 @@
+﻿
+
+//criando uma classe pública chamada ContaCorrente
+public class ContaCorrente
+{
+    public string titular;
+    public int agencia;
+    public int numero;
+    public double saldo = 100;
+
+
+    //criando a função Sacar
+    public bool Sacar(double valor)
+    {
+        if (this.saldo < valor)
+        {
+            return false;
+        }
+        else
+        {
+            this.saldo -= valor;
+            return true;
+        }
+    }
+}
---
+++ 04-ByteBank/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //instanciando (ou criando um objeto) a partir da classe ContaCorrente
+            ContaCorrente contaDoBruno = new ContaCorrente();
+
+            //acessando os parâmetros do objeto e atribuindo valores
+            contaDoBruno.titular = "Bruno";
+
+            Console.WriteLine(contaDoBruno.saldo);
+
+            bool resultadoSaque = contaDoBruno.Sacar(50);
+
+            Console.WriteLine(contaDoBruno.saldo);
+            Console.WriteLine(resultadoSaque);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
